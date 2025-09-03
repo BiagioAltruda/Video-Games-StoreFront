bd15e1ce2d35e7d8cbe4fbf08a73cc02005216e4
BiagioAltruda <biagio.altruda02@gmail.com>
2025-09-03 12:06:48 +0200

    uploaded game banners

--- src/main/java/com/store/storefront/game/GameTest.java
+++ src/main/java/com/store/storefront/game/GameTest.java
@@ -1,11 +1,14 @@
 package com.store.storefront.game;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
 
 @Test
     public void test(){
+    assertTrue(true);
 }
 
 }
