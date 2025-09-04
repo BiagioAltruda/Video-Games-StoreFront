bf3f23b9e6a8c2181bb489dfe98e8ed4e9175173
ValerioPiccol <valeriopiccolo20@gmail.com>
2025-09-04 15:14:22 +0200

    aggiusti
    commt

--- src/main/java/com/store/storefront/trending/Trending.java
+++ src/main/java/com/store/storefront/trending/Trending.java
@@ -1,37 +1,28 @@
 package com.store.storefront.trending;  //il mio amato model <3
 
 import java.sql.Date;
-import java.util.List;
-
-<<<<<<< HEAD
-
-
-import jakarta.persistence.Id;
 
 import com.store.storefront.model.Category;
 import com.store.storefront.model.Game;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-=======
-import org.springframework.data.annotation.Id;
 
-import jakarta.persistence.*;
-import com.store.storefront.model.Game;
-import com.store.storefront.model.Category;
->>>>>>> c11ea1cfab97bb19a51f6c38676075f40899f2f9
 
 @Entity
 @Table (name="trending") //connettiamo alla tab del db
 
 public class Trending {
 
-    @jakarta.persistence.Id
+
     @Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto-increment DB
 	@Column(insertable=false, updatable=false)
@@ -42,13 +33,9 @@ public class Trending {
 	@JoinColumn(name = "game_id", nullable = false)
 	private Game game;
 	
-<<<<<<< HEAD
-	@ManyToOne
-	@JoinColumn(name="id_categories")
-=======
+
 	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	@JoinColumn(name = "category_id", nullable = false)
->>>>>>> c11ea1cfab97bb19a51f6c38676075f40899f2f9
 	private Category category;
 	
 	@Column (name="period")
@@ -60,17 +47,11 @@ public class Trending {
        
 	
 	//costruttore pieno
-<<<<<<< HEAD
-	public Trending(Integer id, Game game, Category category, Date period) {
-		this.id = id;
-		this.game = game;
-		this.category = category;
-=======
+
 	public Trending(Integer id, Game game, Category categories, Date period) {
 		this.id = id;
 		this.game = game;
 		this.category = categories;
->>>>>>> c11ea1cfab97bb19a51f6c38676075f40899f2f9
 		this.period = period;
 	}
 
@@ -95,20 +76,14 @@ public class Trending {
 	}
 
 
-<<<<<<< HEAD
-	public Category getCategories() {
-=======
+
 	public Category getCategory() {
->>>>>>> c11ea1cfab97bb19a51f6c38676075f40899f2f9
 		return category;
 	}
 
 
-<<<<<<< HEAD
-	public void setCategories(Category category) {
-=======
+
 	public void setCategory(Category category) {
->>>>>>> c11ea1cfab97bb19a51f6c38676075f40899f2f9
 		this.category = category;
 	}
 
