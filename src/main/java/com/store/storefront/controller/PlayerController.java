16286ec189732bf30c7864fe9c4565aa6288e9ed
BiagioAltruda <biagio.altruda02@gmail.com>
2025-09-09 10:23:44 +0200

    ignore .env

--- src/main/java/com/store/storefront/controller/PlayerController.java
+++ src/main/java/com/store/storefront/controller/PlayerController.java
@@ -1,15 +1,19 @@
 package com.store.storefront.controller;
 
 import java.util.List;
-import java.util.Map;
-
 import com.store.storefront.model.Player;
 import com.store.storefront.service.PlayerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*") // Consente richieste CORS da qualsiasi origine (solo per sviluppo)
 @RestController // Indica che questa classe gestisce richieste REST
 @RequestMapping("/api/player") // Prefisso comune per tutti gli endpoint 
@@ -17,8 +21,6 @@ public class PlayerController {
 		
 	//DEPENDENCY INJECTION
 	private final PlayerService service;
-
-
 	
 	@Autowired
 	public PlayerController (PlayerService service) {
