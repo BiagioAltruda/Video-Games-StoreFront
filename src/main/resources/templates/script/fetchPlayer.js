4349216d08ab30de835834098c92b870d4db09e2
BiagioAltruda <biagio.altruda02@gmail.com>
2025-09-11 11:34:47 +0200

    added fetchPlayer.js script to get player id from token

---
+++ src/main/resources/templates/script/fetchPlayer.js
@@ -0,0 +1,20 @@
+export async function getPlayerId() {
+    let token = localStorage.getItem('token');
+    let options = {method : 'GET' , headers : {'X-Token': token}};
+    try {
+        const response = await fetch(`http://localhost:8080/smoke/accounts/profile`, options);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const playerId = data.id;
+
+        console.log(`The player ID is: ${playerId}`);
+        return playerId; // You can now return the ID from the function
+    } catch (err) {
+        console.error("Failed to fetch player ID:", err);
+        return null; // Or throw the error to the caller
+    }
+}
